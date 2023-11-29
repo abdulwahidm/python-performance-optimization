e912518e30f936421a471684c58ae42883445505
Abdul Wahid Muhaemin <wamhabdul@gmail.com>
2023-11-30 00:37:44 +0700

    measuring time execution

---
+++ measuring-time-execution/main.py
@@ -0,0 +1,20 @@
+import time
+
+print("\nHello...\n")
+
+def long_time():
+    x = 0
+    for j in range(0, 10000):
+        for i in range(0, 10000):
+            x += 1
+    return x        
+
+start = time.time()
+
+long_time()
+
+end = time.time()
+
+print(long_time())
+
+print(end - start)
