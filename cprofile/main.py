5b5ac0238002e920eb2fd60c1d6b8601f614726b
Abdul Wahid Muhaemin <wamhabdul@gmail.com>
2023-11-30 14:08:10 +0700

    docs : add more detail about cProfile output

--- cprofile/main.py
+++ cprofile/main.py
@@ -25,4 +25,12 @@ yang dihabiskan di dalamnya. Hasilnya mungkin terlihat seperti ini:
         1    0.000    0.000    0.118    0.118 {built-in method builtins.print}
         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
+        
+Dalam output ini, Anda dapat melihat informasi seperti jumlah panggilan fungsi (ncalls), 
+waktu total yang dihabiskan dalam fungsi itu sendiri (tottime), dan waktu kumulatif 
+yang mencakup waktu di fungsi itu sendiri dan fungsi yang dipanggilnya (cumtime).
+
+Profilisasi kode dengan cProfile sangat berguna saat Anda mencari area 
+yang memerlukan optimasi untuk meningkatkan kinerja aplikasi Anda.
+
 """
