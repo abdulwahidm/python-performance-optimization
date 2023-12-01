af71c448453268e01f048e2c4d68f019411513e2
Abdul Wahid Muhaemin <wamhabdul@gmail.com>
2023-12-02 00:05:30 +0700

    feat: add vproof implements

--- measuring-time-execution/main.py
+++ measuring-time-execution/main.py
@@ -1,11 +1,11 @@
 import time
 
-print("\nHello...\n")
+print("\nTest...\n")
 
 def long_time():
     x = 0
-    for j in range(0, 1_000):
-        for i in range(0, 1_000):
+    for j in range(0, 1_0000):
+        for i in range(0, 1_0000):
             x += 1
     return x        
 
---
+++ vprof/main.py
@@ -0,0 +1,15 @@
+# main.py
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+def main():
+    result = fibonacci(30)
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
